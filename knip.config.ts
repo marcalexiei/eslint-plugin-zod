d5774cc37d92bed4f4a9ea73599ad8c91c193017
Marco Pasqualetti <marco.pasqualetti@live.com>
2026-04-14 07:07:38 +0200

    chore(knip): improve config

--- knip.config.ts
+++ knip.config.ts
@@ -1,8 +1,10 @@
 import type { KnipConfig } from 'knip';
 
 export default {
-  tags: ['-lintignore'],
-  ignore: [
-    '.eslint-doc-generatorrc.js', // Executed by `eslint-doc-generator`
+  entry: [
+    // 'src/index.ts',
+
+    // Executed by `eslint-doc-generator`
+    '.eslint-doc-generatorrc.js',
   ],
 } satisfies KnipConfig;
